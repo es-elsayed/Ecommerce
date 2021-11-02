064aa15f0185a541a0566dfed75bb5019ae73a90
islam-abdelkader <islamabdelkader22@gmail.com>
2021-11-02 13:13:20 +0200

    attach front end theme

---
+++ resources/views/front/includes/scripts.blade.php
@@ -0,0 +1,12 @@
+	<!-- Bootstrap JS -->
+	<script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
+	<!--plugins-->
+	<script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+	<script src="{{ asset('assets/plugins/simplebar/js/simplebar.min.js') }}"></script>
+	<script src="{{ asset('assets/plugins/OwlCarousel/js/owl.carousel.min.js') }}"></script>
+	<script src="{{ asset('assets/plugins/OwlCarousel/js/owl.carousel2.thumbs.min.js') }}"></script>
+	<script src="{{ asset('assets/plugins/metismenu/js/metisMenu.min.js') }}"></script>
+	<script src="{{ asset('assets/plugins/perfect-scrollbar/js/perfect-scrollbar.js') }}"></script>
+	<!--app JS-->
+	<script src="{{ asset('assets/js/app.js') }}"></script>
+	<script src="{{ asset('assets/js/index.js') }}"></script>
